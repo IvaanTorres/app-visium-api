5ffbc1afe08b9a877338145ea22cd789b3aba5e4
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-08-26 21:25:29 +0200

    feat(security): create token secrets, password hashing and HMAC system for jwt

--- app/main.py
+++ app/main.py
@@ -7,41 +7,31 @@ import json
 import hashlib
 from typing import Dict
 import datetime
-
-
 import secrets
 
 app = FastAPI()
 
 # ---------------------------------- SECRETS --------------------------------- #
-def encrypt(encryption_key, plaintext):
-    encrypted = []
-    for i in range(len(plaintext)):
-        key_char = encryption_key[i % len(encryption_key)]
-        encrypted_char = chr(ord(plaintext[i]) ^ ord(key_char))
-        encrypted.append(encrypted_char)
-    return ''.join(encrypted)
-
-def decrypt(decryption_key, ciphertext):
-    decrypted = []
-    for i in range(len(ciphertext)):
-        key_char = decryption_key[i % len(decryption_key)]
-        decrypted_char = chr(ord(ciphertext[i]) ^ ord(key_char))
-        decrypted.append(decrypted_char)
-    return ''.join(decrypted)
+def generate_secret_key():
+    # Random URL-safe string of 32 bytes
+    return secrets.token_urlsafe(32)
 
 # ------------------------------------ JWT ----------------------------------- #
 def encode_base64(data: bytes) -> str:
     base64_bytes = base64.urlsafe_b64encode(data)
     return base64_bytes.decode('ascii').rstrip("=")
 
-def create_jwt(header: Dict, payload: Dict) -> str:
+def generate_jwt_signature(secret_key, data):
+    signature = hmac.new(secret_key.encode('utf-8'), data.encode('utf-8'), hashlib.sha256)
+    return base64.urlsafe_b64encode(signature.digest()).decode('utf-8')
+
+def create_jwt(header: Dict, payload: Dict, secret: str) -> str:
     encoded_header = encode_base64(json.dumps(header).encode('utf-8'))
     encoded_payload = encode_base64(json.dumps(payload).encode('utf-8'))
 
-    signature_input = f"{encoded_header}.{encoded_payload}".encode('ascii')
-    signature = hmac.new(generate_jwt(), signature_input, hashlib.sha256)
-    encoded_signature = encode_base64(signature.digest())
+    signature_input = f"{encoded_header}.{encoded_payload}"
+    signature = generate_jwt_signature(secret, signature_input)
+    encoded_signature = encode_base64(signature.encode('utf-8'))
 
     jwt = f"{encoded_header}.{encoded_payload}.{encoded_signature}"
     return jwt
@@ -54,19 +44,17 @@ def validate_jwt(token, secret):
         # Decode the header and payload
         header = json.loads(base64.urlsafe_b64decode(header_base64 + "==").decode("utf-8"))
         payload = json.loads(base64.urlsafe_b64decode(payload_base64 + "==").decode("utf-8"))
-
         # Verify the signature
-        encoded_signature_input = f"{header_base64}.{payload_base64}".encode("utf-8")
-        expected_signature = hmac.new(secret, encoded_signature_input, hashlib.sha256).digest()
+        encoded_signature_input = f"{header_base64}.{payload_base64}"
+        expected_signature = generate_jwt_signature(secret, encoded_signature_input)
         actual_signature = base64.urlsafe_b64decode(signature + "==")
 
 
-        if not hmac.compare_digest(expected_signature, actual_signature):
+        if not hmac.compare_digest(expected_signature.encode('utf-8'), actual_signature):
             print("Invalid signature")
             return False
         
         print("Valid signature")
-
 
         # Check expiration
         current_time = datetime.datetime.utcnow()
@@ -88,37 +76,30 @@ def validate_password(password: str, hashed_password: str) -> bool:
 
 
 @app.get("/")
-def generate_jwt():
+def generate_jwt(duration: int = 1):
 
     # TODO: JWT (HMAC-SHA256 MAC)
-    # Example header and payload (you can customize these)
-    # header = {
-    #     "alg": "HS256",  # Algorithm used for signature
-    #     "typ": "JWT"     # Type of token
-    # }
-    # payload = {
-    #     "sub": "user123",  # Subject
-    #     "exp": 1672531200  # Expiration time (example: 01/01/2023)
-    # }
+    current_time = datetime.datetime.utcnow()
+    expiration_time = current_time + datetime.timedelta(hours=duration)
 
-    # jwt = create_jwt(header, payload)
-    # validated_jwt = validate_jwt(jwt, SECRET_KEY)
+    header = {
+        "alg": "HS256", 
+        "typ": "JWT"     
+    }
+    payload = {
+        "sub": "user123",
+        "exp": int(expiration_time.timestamp())
+    }
 
-    # return {
-    #     "jwt": jwt,
-    #     "validated_jwt": validated_jwt
-    # }
+    # IMPORTANT: Store the random secret into secure secrets management system or a secure database as HashiCorp Vault, AWS Secrets Manager, or a secure database
+    secret = generate_secret_key()
+    jwt = create_jwt(header, payload, secret)
+    validated_jwt = validate_jwt(jwt, secret)
 
-    # TODO: SECRETS (ENCRYPTION)
-    # Example usage
-    password = "1234"
-    jwt_secret = "Secret"
-
-    encrypted_data = encrypt(password, jwt_secret)
-    print("Encrypted:", encrypted_data)
-
-    decrypted_data = decrypt(password, encrypted_data)
-    print("Decrypted:", decrypted_data)
+    return {
+        "jwt": jwt,
+        "validated_jwt": validated_jwt
+    }
 
     # TODO: PASSWORD (HASHING)
     # password = "123456"
--- app/schemas/user.py
+++
@@ -1,3 +0,0 @@
-from typing import Optional, List, Generic, TypeVar
-from pydantic import BaseModel, EmailStr, Field, validator
-from pydantic.generics import GenericModel
