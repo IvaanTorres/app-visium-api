5d06383a053675a1d1005f23b865669bd4022cb8
IvaanTorres <ivantorresgarcia.02@gmail.com>
2023-08-27 20:12:02 +0200

    feat(general): get welcoming message size (#41)

--- app/main.py
+++ app/main.py
@@ -451,6 +451,34 @@ def get_info_login(request: Request):
     except Exception as e:
         return {"error": e}
 
+@app.get("/settings/general")
+def get_general_settings(request: Request):
+    try:
+        refresh_token_with_bearer = request.headers["Authorization"]
+        refresh_token = refresh_token_with_bearer.split(" ")[1]
+
+        refresh_token_payload = check_access(refresh_token, SECRET_KEY)
+
+        if refresh_token_payload:
+            user_id = refresh_token_payload["user_id"]
+
+            session = SessionLocal()
+            storedUser = session.query(User).filter(User.id == user_id).first()
+            if not storedUser:
+                raise HTTPException(status_code=400, detail="User does not exist")
+
+            storedPreference = session.query(Preference).filter(Preference.user_id == user_id).first()
+
+            session.close()
+
+            return {
+                "message": "User general settings retrieved successfully",
+                "welcomingMessageSize": storedPreference.welcomingMessageSize
+            }
+
+    except Exception as e:
+        return {"error": e}
+
 
 # Server running 
 if __name__ == "__main__ ":
