853466aa676b2b2bdc45e5772283395be5a8e19d
Ivan Torres Garcia <itorres@sinfin.fr>
2023-08-28 16:53:30 +0200

    style(setup): move setup + cors middleware inside the middlewares folder (#47)

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
 import uvicorn
 from .db.config import engine, Base
-from fastapi.middleware.cors import CORSMiddleware
+
+from .middlewares.setup import setup_app
 
 from .api.user import router as user_router
 from .api.settings.profile import router as settings_profile_router
@@ -13,25 +13,12 @@ from .api.login_infos import router as login_infos_router
 # Since I am not able to access for the specific user secret key from those services, I'll use the same for everyone.
 # However, it's not a good practice to do so.
 # It'd be better to fetch the user's secret key from the secrets management system and use it to generate the tokens.
+
 # SECRET_KEY = generate_secret_key() # IMPORTANT: Everytime you restart the server, the change of secret will make the active tokens unusable
 # SECRET_KEY = 'my-secret-key' will be same for everyone even if you restart the server
 
-# Create the FastAPI instance
-app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
-
-# Configure CORS
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
+# Create the FastAPI instance + CORS middleware
+app = setup_app()
 
 # Run migrations
 Base.metadata.create_all(bind=engine)
---
+++ app/middlewares/setup.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+def setup_app():
+    app = FastAPI()
+
+    origins = [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
+
+    # Configure CORS
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"]
+    )
+
+    return app
